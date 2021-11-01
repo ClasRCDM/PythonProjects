8ee9c223075010e9c732dc5b8301e8ea56e943fb
Clas_RCDM <raphaelcalixto2013@gmail.com>
2021-11-01 15:20:23 -0400

    Reoganização geral do código

--- MyPacman/Oh_Pacman.py
+++ MyPacman/Oh_Pacman.py
@@ -27,33 +27,23 @@ class World:  # A World l
         self.Run = None
         self.running = None
 
-        self.font = None
-
         self.point_mouse = None
         # &#########################& #
 
         #################
         # \Itens World/ #
 
+        self.Itens_world = {}
+
         # $ Sprites $ #
-        self.Sprites_world = None
-
         # $ Directories $ #
-        self.dirctrymges = None
-        self.dirctyudo = None
-
         # $ Scenes $ #
-        self.Game_Start = None
-        self.Game = None
-
         # $ WidGets $ #
-        self.widget_world = None
-
         # $ Images $ #
-        self.pacmanlogo = None
-
         # $ Texts $ #
-        self.Start_text = None
+
+        # \Itens World/ #
+        #################
 
     def World_init(self):
         # Set itens world
@@ -77,12 +67,12 @@ class World:  # A World l
 
     def World_objcts(self):
         # Add in World
-        self.Sprites_world = pygame.sprite.Group()
+        self.Itens_world['Sprites_world'] = pygame.sprite.Group()
 
         self.World_time()
 
     def World_widget(self):
-        match self.widget_world:
+        match self.Itens_world['widget_world']:
             case 'StartGame':
                 self.World_text_menu_update()
                 self.World_sounds()
@@ -94,7 +84,8 @@ class World:  # A World l
                 self.World_sounds()
 
     def World_widget_events(self, ev):
-        self.World_text_menu_events(ev)
+        if self.Itens_world['Game_Start'] and not self.Itens_world['Game']:
+            self.World_text_menu_events(ev)
 
     def World_time(self):
         # Time and Space world
@@ -125,7 +116,11 @@ class World:  # A World l
 
             # Init game
             if ev.type == pygame.KEYUP:
-                self.Game, self.Game_Start = True, False
+                self.Itens_world['Game'] = True
+                self.Itens_world['Game_Start'] = False
+
+                del self.Itens_world['pacmanlogo']
+                del self.Itens_world['Start_text']
 
                 v.MUSICS = 'PlayGame_music'
                 v.FPS = 60
@@ -133,45 +128,50 @@ class World:  # A World l
             self.World_widget_events(ev)
 
     def World_functions(self):
-        if self.Game_Start and not self.Game:
-            self.widget_world = 'StartGame'
-        elif self.Game and not self.Game_Start:
-            self.widget_world = 'PlayGame'
+        if self.Itens_world['Game_Start'] and not self.Itens_world['Game']:
+            self.Itens_world['widget_world'] = 'StartGame'
+        elif self.Itens_world['Game'] and not self.Itens_world['Game_Start']:
+            self.Itens_world['widget_world'] = 'PlayGame'
 
     def World_scenes(self):
         # Manage the scenes
-        self.Game_Start = v.game_start
+        self.Itens_world['Game_Start'] = v.game_start
 
-        self.Game = v.game
+        self.Itens_world['Game'] = v.game
 
     def World_adofle(self):
         # Load files, and audios
-        self.dirctrymges = path.join(getcwd(), v.MAIN_FILE, v.FILES[0])
-        self.dirctyudo = path.join(getcwd(), v.MAIN_FILE, v.FILES[1])
-        self.dirctyfnts = path.join(getcwd(), v.MAIN_FILE, v.FILES[2])
+        self.Itens_world['dirctrymges'] = path.join(
+            getcwd(), v.MAIN_FILE, v.FILES[0])
+        self.Itens_world['dirctyudo'] = path.join(
+            getcwd(), v.MAIN_FILE, v.FILES[1])
+        self.Itens_world['dirctyfnts'] = path.join(
+            getcwd(), v.MAIN_FILE, v.FILES[2])
 
-        self.font_set = path.join(self.dirctyfnts, v.FONT)
+        self.font_set = path.join(self.Itens_world['dirctyfnts'], v.FONT)
 
     def World_images(self):
         # Call/add image
-        pacmanlogo = path.join(self.dirctrymges, v.LOGO_PACMAN)
-        self.pacmanlogo = pygame.image.load(pacmanlogo).convert_alpha()
+        pacmanlogo = path.join(self.Itens_world['dirctrymges'], v.LOGO_PACMAN)
+        self.Itens_world['pacmanlogo'] = pygame.image.load(
+            pacmanlogo).convert_alpha()
 
-        match self.widget_world:
+        match self.Itens_world['widget_world']:
             case 'StartGame':
-                self.screen.blit(self.pacmanlogo, (6, 0))
+                self.screen.blit(self.Itens_world['pacmanlogo'], (6, 0))
 
     def World_sounds(self):
         # Call/add sound
         match v.MUSICS:
             case 'StartGame_music':
-                start_dirc = path.join(self.dirctyudo, v.music_START)
+                start_dirc = path.join(
+                    self.Itens_world['dirctyudo'], v.music_START)
                 s.sound(start_dirc, 0.1, True)
 
                 v.MUSICS = ''
 
             case 'PlayGame_music':
-                play = path.join(self.dirctyudo, v.music_PLAY)
+                play = path.join(self.Itens_world['dirctyudo'], v.music_PLAY)
                 s.sound(play, play=True)
                 pygame.mixer.fadeout(500)
 
@@ -179,7 +179,7 @@ class World:  # A World l
 
             case 'Background_music':
                 background_music_dirc = path.join(
-                    self.dirctyudo, v.music_BACKGROUND)
+                    self.Itens_world['dirctyudo'], v.music_BACKGROUND)
 
                 s.sound(background_music_dirc, 0.5, play=True, loop=True)
 
@@ -187,35 +187,37 @@ class World:  # A World l
 
     def World_sprits(self):
         # Set/Update sprits/draw
-        self.Sprites_world.update()
+        self.Itens_world['Sprites_world'].update()
 
     def World_text_menu(self):
         # Load/render texts
-        self.Start_text = t.text(self.screen, self.font_set, v.text_START, 15,
-                                 v.YELLOW, v.WIDTH / 2, 320, True)  # Press a key to play
+        self.Itens_world['Start_text'] = t.text(
+            self.screen, self.font_set, v.text_START, 15,
+            v.YELLOW, v.WIDTH / 2, 320, True)  # Press a key to play
 
     def World_text_menu_events(self, ev):
-        self.Start_text.point(pygame.mouse.get_pos(), ev)
+        self.Itens_world['Start_text'].point(pygame.mouse.get_pos(), ev)
 
     def World_text_menu_update(self):
         # Update texts
-        self.Start_text.animation(True, 1, 3, '+zoon', 'one_click', 'Touched')
-        self.Start_text.draw()
+        self.Itens_world['Start_text'].animation(
+            True, 1, 3, '+zoon', 'one_click', 'Touched')
+        self.Itens_world['Start_text'].draw()
 
         t.text(self.screen, self.font_set, '-Desenvolvido por',
-               12, v.WHITE, v.WIDTH / 2 - 65, 520, True)
+               12, v.WHITE, v.WIDTH / 2 - 65, 520, True)  # Developed
         t.text(self.screen, self.font_set, 'ClasRCDM-',
-               12, v.WHITE, v.WIDTH / 2 + 105, 520, True)  # Developed by ClasRCDM
+               12, v.WHITE, v.WIDTH / 2 + 105, 520, True)  # by ClasRCDM
 
         t.text(self.screen, self.font_set, '-Projeto inspirado por',
-               11, v.GREY, v.WIDTH / 2 - 75, 550, True)
+               11, v.GREY, v.WIDTH / 2 - 75, 550, True)  # Project inspired
         t.text(self.screen, self.font_set, 'João Tinti-',
-               11, v.GREY, v.WIDTH / 2 + 115, 550, True)  # Project inspired by João Tinti
+               11, v.GREY, v.WIDTH / 2 + 115, 550, True)  # by João Tinti
 
     def World_SpritsDraw(self):
         # Add/set draw about sprits
         self.screen.fill(v.BACKGROUND)
-        self.Sprites_world.draw(self.screen)
+        self.Itens_world['Sprites_world'].draw(self.screen)
 
 
 ###################
